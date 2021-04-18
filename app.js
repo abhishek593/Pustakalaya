b5eacb70c0363f8bc69f3a5bd229cfdc380a0c60
Abhishek <cse190001001@iiti.ac.in>
2021-04-18 17:14:38 +0530

    Added librarian functions

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var con= mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Prasheel@4",
+    password: "5930",
     database: "pustakalaya"
 })
 con.connect(function(err) {
@@ -24,6 +24,25 @@ con.connect(function(err) {
     console.log('Connection established');
 });
 
+con.promise = (sql) => {
+    return new Promise((resolve, reject) => {
+        con.query(sql, (err, result) => {
+            if(err){reject(new Error());}
+            else{resolve(result);}
+        });
+    });
+};
+
+function getSQLResult(sql) {
+    let value = "";
+    con.promise(sql)
+        .then((result) => {
+            value = result;
+        }).catch((err) => {
+            console.log(err);
+    })
+    return value;
+}
 ///////////////////////////////////////////////////////////////////////////////
 
 const mongoose= require("mongoose");
@@ -62,32 +81,56 @@ app.get("/login",function(req,res){
     res.render("login");
 })
 
-let count=1;
-
 app.get("/register",function(req,res){
     res.render("register");
 })
 
+let count = 0;
+
 app.post("/register",function(req,res){
+
+    con.connect(function (err) {
+        con.query("SELECT * FROM users", function (err, result) {
+            count += result.length;
+            // console.log(result);
+            // console.log("res length = " + result.length);
+        });
+        // console.log(count);
+    })
+
+    count += 1;
+    console.log("count = " + count);
+    let role = req.body.role;
+    let l = role[0] + count;
+    console.log("l = " + l);
+
     console.log(req.body.username, req.body.password,req.body)
-    let role=req.body.role;
-    let l=role[0]+count;
-    console.log(l);
-    count+=1;
+    // console.log(l);
     User.register({username: req.body.username,id: l},req.body.password,function(err,user){
         if(err){
-            console.log(err);
+            // console.log(err);
             res.redirect("/register");
         }
         else{
             passport.authenticate("local")(req,res,function(){
+
                 console.log(user.id);
                 con.connect(function(err) {
-                    var sql = 'Insert into users value("'+user.id+'")';
-                    console.log(sql);
+                    let sql = 'Insert into users value("'+user.id+'")', sql2;
+                    if (role === "faculty") {
+                        sql2 = `INSERT INTO faculty (name, email, password, address, f_id) VALUES("${req.body.name}", "${req.body.username}", "${req.body.password}", 
+                        "${req.body.address}", "${l}")`;
+                    }else{
+                        sql2 = `INSERT INTO student (name, email, password, address, s_id) VALUES("${req.body.name}", "${req.body.username}", "${req.body.password}", 
+                        "${req.body.address}", "${l}")`;
+                    }
                     con.query(sql ,function (err, result) {
                       if (err) throw err;
                       console.log(result);
+                    })
+                    con.query(sql2 ,function (err, result) {
+                        if (err) throw err;
+                        console.log(result);
                     })
                 })
                 res.redirect("/dashboard/search_book");
@@ -103,7 +146,7 @@ app.get("/logout",function(req,res){
 app.post("/login",function(req,res){
     const user= new User({
         username: req.body.username,
-        password: req.body.passoword
+        password: req.body.password
     });
     
     req.login(user,function(err){
@@ -203,6 +246,61 @@ app.post("/hold_request",function(req,res){
 
 })
 
+
+/*
+Librarian stuff starts
+In all these cases we want to add a check if id starts from l or not
+ */
+app.get("/librarian_special", function (req, res) {
+    if (!req.isAuthenticated()) res.redirect("/login");
+    return res.render("librarian_special_dashboard");
+})
+
+const ON_LOAN = "on-loan";
+const ON_SHELF = "on-shelf";
+const ON_HOLD = "on-hold";
+const ON_LOAN_AND_HOLD = "on-loan-and-on-hold";
+
+app.get("/addBook", function (req, res) {
+    return res.render("add_book");
+})
+
+app.post("/addBook", function (req, res) {
+    if (!req.isAuthenticated()) res.redirect("/login");
+    let sql =  `INSERT INTO books_collection (ISBN, title, author, year_of_pub, shelf_id, current_status, present_status) VALUES
+            ("${req.body['ISBN']}", "${req.body['title']}", "${req.body['author']}", "${req.body['year_of_pub']}", "${req.body['shelf_id']}",
+            "${ON_SHELF}", "${1}")`;
+    getSQLResult(sql);
+    let result = getSQLResult(`SELECT * FROM all_books WHERE ISBN = "${req.body['ISBN']}"`);
+    let x = 0;
+    if (result.length > 0) x = result.length;
+    x += 1;
+    console.log("x = " + x);
+    sql = `INSERT INTO all_books (ISBN, copy_number, status) VALUES ("${req.body['ISBN']}", "${x}", "${1}")`;
+    console.log(getSQLResult(sql));
+    return res.render("librarian_special_dashboard");
+})
+
+app.get("/deleteBook", function (req, res) {
+    return res.render("add_book");
+})
+
+app.post("/deleteBook", function (req, res) {
+    if (!req.isAuthenticated()) res.redirect("/login");
+})
+
+app.get("/updateBook", function (req, res) {
+    return res.render("add_book");
+})
+
+app.post("/updateBook", function (req, res) {
+    if (!req.isAuthenticated()) res.redirect("/login");
+})
+/*
+Librarian stuff ends
+ */
+
 app.listen(process.env.PORT || port,function(){
     console.log("Server running at port "+port);
+
 })
